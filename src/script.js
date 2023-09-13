47a861ba5295ec282ab0f0d0ced36b4c1e5f14ab
loandre <loandre.dev@gmail.com>
2023-09-13 03:34:07 -0300

    fixed-eye

--- src/script.js
+++ src/script.js
@@ -27,7 +27,7 @@ pwShowHide.forEach(icon => {
             icon.classList.replace("uil-eye-slash", "uil-eye");
         }else{
             getPwInput.type = "password";
-            icon.classList.replace("uil-eye-slash", "uil-eye");
+            icon.classList.replace("uil-eye", "uil-eye-slash");
         }
     });
 });
