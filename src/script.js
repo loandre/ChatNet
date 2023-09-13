e3f993b2af5b29816f5cf46c91d8a9788b6f85bd
loandre <loandre.dev@gmail.com>
2023-09-12 22:29:24 -0300

    updtate-css

--- src/script.js
+++ src/script.js
@@ -1,13 +1,50 @@
-const formOpenBtn = document.querySelector("#form-open"),
-    home = document.querySelector(".home"),
-    formContainer = document.querySelector(".form_container"),
-    formCloseBtn = document.querySelector(".form_close"),
-    signupBtn = document.querySelector("#signup"),
-    loginBtn = document.querySelector("#signup"),
-    pwShowHide = document.querySelectorAll(".pw_hide");
+const menu = document.querySelector('.menu');
+const navMenu = document.querySelector('.nav-menu');
+const formOpenBtn = document.querySelector("#form-open");
+const home = document.querySelector(".home");
+const overlay = document.getElementById("overlay");
+const formContainer = document.querySelector(".form_container");
+const formCloseBtn = document.querySelector(".form_close");
+const signupBtn = document.querySelector("#signup");
+const loginBtn = document.querySelector("#login");
+const pwShowHide = document.querySelectorAll(".pw_hide");
 
-    formOpenBtn.addEventListener("click", () => home.classList.add("show"))
-    formCloseBtn.addEventListener("click", () => home.classList.remove("show"))
+formOpenBtn.addEventListener("click", () => {
+    home.classList.add("show");
+    overlay.style.display = "block"; 
+});
+
+formCloseBtn.addEventListener("click", () => {
+    home.classList.remove("show");
+    overlay.style.display = "none";
+});
+
+pwShowHide.forEach(icon => {
+    icon.addEventListener("click", () => {
+        let getPwInput = icon.parentElement.querySelector("input");
+        if (getPwInput.type === "password"){
+            getPwInput.type = "text";
+            icon.classList.replace("uil-eye-slash", "uil-eye");
+        }else{
+            getPwInput.type = "password";
+            icon.classList.replace("uil-eye-slash", "uil-eye");
+        }
+    });
+});
+
+signupBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    formContainer.classList.add("active");
+});
+loginBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    formContainer.classList.remove("active");
+});
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('ativo');
+    navMenu.classList.toggle('ativo');
+});
 
 
 
@@ -16,10 +53,7 @@ const formOpenBtn = document.querySelector("#form-open"),
 
 
 
-// const menu = document.querySelector('.menu');
-// const NavMenu = document.querySelector('.nav-menu');
 
-// menu.addEventListener('click', () => {
-//     menu.classList.toggle('ativo');
-//     NavMenu.classList.toggle('ativo');
-// })
+
+
+
